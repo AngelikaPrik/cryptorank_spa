5cca1ec76bdc0b379f9247ec08c39d7aaaa9cfab
Angelina Mamakova <pretzel318@gmail.com>
2023-07-20 17:24:03 +0400

    add tests for converter

--- __tests__/components/converter.test.tsx
+++ __tests__/components/converter.test.tsx
@@ -1,9 +1,14 @@
 import { Converter } from '@/components/Converter'
-import { render, waitFor } from '@testing-library/react'
+import {
+  fireEvent,
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 
 describe('Converter: ', () => {
   test('should render converter components', () => {
-    const { getByTestId } = render(<Converter />)
+    const { getByTestId, getByText } = render(<Converter />)
 
     waitFor(() => {
       expect(getByTestId('amount-input')).toBeInTheDocument()
@@ -11,6 +16,39 @@ describe('Converter: ', () => {
       expect(getByTestId('to')).toBeInTheDocument()
       expect(getByTestId('flipCurrency')).toBeInTheDocument()
       expect(getByTestId('result')).toBeInTheDocument()
+    })
+    waitForElementToBeRemoved(getByTestId('loader'))
+
+    jest.mock('axios', () => ({
+      get: jest.fn(() => Promise.reject(new Error('Failed to fetch data'))),
+    }))
+
+    waitFor(() => {
+      expect(getByText('Error loading data')).toBeInTheDocument()
+    })
+
+    waitFor(() => {
+      const amountInput = getByTestId('amount-input') as HTMLInputElement
+      fireEvent.change(amountInput, { target: { value: '1' } })
+      expect(amountInput.value).toBe('1')
+    })
+
+    waitFor(() => {
+      expect(getByText('0 BTC = 0 USD')).toBeInTheDocument()
+    })
+
+    waitFor(() => {
+      const flipCurrencyButton = getByTestId('flipCurrency')
+
+      expect(flipCurrencyButton).toBeInTheDocument()
+      expect(flipCurrencyButton).toBeEnabled()
+
+      fireEvent.click(flipCurrencyButton)
+    })
+
+    waitFor(() => {
+      expect((getByTestId('from') as HTMLSelectElement).value).toBe('USD')
+      expect((getByTestId('to') as HTMLSelectElement).value).toBe('BTC')
     })
   })
 })
