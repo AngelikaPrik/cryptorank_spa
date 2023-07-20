7ea90791e128be1da7209f7654da57ed88475d66
Angelina Mamakova <pretzel318@gmail.com>
2023-07-20 12:05:59 +0400

    removed fetcher & minor changes with style table

--- src/app/cryptoPrices/page.tsx
+++ src/app/cryptoPrices/page.tsx
@@ -1,18 +1,20 @@
 'use client'
-import useSWR, { Fetcher } from 'swr'
-import axios from 'axios'
+import useSWR from 'swr'
 import { styled } from 'styled-components'
 import { formatNumber } from '@/utils'
 import { CurrencyListData } from '@/types'
 import { COINDATA_API_URL } from '@/constants'
 import { Loader } from '@/components/Loader'
+import { fetcher } from '@/api/fetcher'
 
 const Container = styled.div`
   max-width: 80%;
   margin: 0 auto;
+  @media (max-width: 768px) {
+    max-width: 100%;
+  }
 `
 const Table = styled.table`
-  margin: 10px;
   width: 100%;
   border-radius: 10px;
   background-color: #fff;
@@ -36,22 +38,34 @@ const Table = styled.table`
   td:first-child {
     padding-left: 30px;
   }
+
+  @media (max-width: 768px) {
+    font-size: 14px;
+    th,
+    td {
+      padding: 10px;
+    }
+  }
+
+  @media (max-width: 480px) {
+    font-size: 12px;
+  }
 `
-const fetcher: Fetcher<CurrencyListData, string> = async (url: string) => {
-  const { data } = await axios.get(url)
-  return data
-}
 
 export default function CryptoPrices() {
-  const { data, error } = useSWR<CurrencyListData>(COINDATA_API_URL, fetcher)
+  const { data, error, isLoading } = useSWR<CurrencyListData, Error>(
+    COINDATA_API_URL,
+    fetcher
+  )
 
   if (error) return <Container>Error loading data</Container>
-  if (!data)
+  if (isLoading || !data) {
     return (
       <Container>
         <Loader />
       </Container>
     )
+  }
 
   return (
     <Container>
