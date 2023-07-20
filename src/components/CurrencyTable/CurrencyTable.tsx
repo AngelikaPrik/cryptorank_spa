7cc68fdda657b7041a4f9cc506b46c2814dc32f8
Angelina Mamakova <pretzel318@gmail.com>
2023-07-20 12:45:51 +0400

    create currency table

---
+++ src/components/CurrencyTable/CurrencyTable.tsx
@@ -0,0 +1,35 @@
+import { formatNumber } from '@/utils'
+import { CurrencyListData } from '@/types'
+import { Table } from '../ui/Table'
+
+export const CurrencyTable = ({ data }: PropsType) => {
+  const theads = [
+    'Name',
+    'Price USD',
+    'Circulating Supply',
+    'Market Cap',
+    'Category',
+  ]
+
+  return (
+    <Table theads={theads}>
+      {data.data.map(
+        ({ name, category, circulatingSupply, values, symbol }) => (
+          <tr key={circulatingSupply}>
+            <td>{name}</td>
+            <td>$ {formatNumber(values.USD.price)}</td>
+            <td>
+              {symbol} {formatNumber(circulatingSupply)}
+            </td>
+            <td>$ {formatNumber(values.USD.marketCap)}</td>
+            <td>{category}</td>
+          </tr>
+        )
+      )}
+    </Table>
+  )
+}
+
+interface PropsType {
+  data: CurrencyListData
+}
