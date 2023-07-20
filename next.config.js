c9ad8c7e87caa72ddc64f732fd19779c82e0279c
Angelina Mamakova <pretzel318@gmail.com>
2023-07-20 14:10:49 +0400

    ignore test files

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    forceSwcTransforms: true,
-  },
   env: {
     customKey: '68ac41747d6e0ece93db0d27975114d9a3515e8270964c1044e69e46c2a9',
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.test.js$/,
+      loader: 'ignore-loader',
+    })
+    return config
   },
 }
 
