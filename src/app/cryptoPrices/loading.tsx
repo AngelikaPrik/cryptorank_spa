322626905d35eb521a427120fa7135a12d94778b
Angelina Mamakova <pretzel318@gmail.com>
2023-07-20 12:45:02 +0400

    added loader in ui directory

--- src/app/cryptoPrices/loading.tsx
+++ src/app/cryptoPrices/loading.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { Loader } from '@/components/Loader'
+import { Loader } from '@/components/ui/Loader'
 import { styled } from 'styled-components'
 
 const Container = styled.div`
