3ee2eaf04cb9076aa52c2546918eafbdd7469fb6
Angelina Mamakova <pretzel318@gmail.com>
2023-07-20 16:06:24 +0400

    change constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,1 @@
-const BASE_URL = 'https://api.cryptorank.io/'
-export const COINDATA_API_URL = `${BASE_URL}v1/currencies?api_key=${process.env.customKey}`
-export const CONVERT_API_URL = `${BASE_URL}v1/currencies/1?api_key=${process.env.customKey}`
+export const BASE_URL = 'https://api.cryptorank.io/'
